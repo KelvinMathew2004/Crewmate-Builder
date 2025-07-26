ff2920ecc6d1d6d0c9e292061d1fb05aae79cef1
Kelvin Mathew <kelvinmathew2004@gmail.com>
2025-07-26 18:52:55 -0500

    Added cards for galleries and their styling

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,15 +1,19 @@
 import './Card.css'
 import { Link } from 'react-router-dom'
+import crewmateOutline from '../assets/crewmate-outline.png'
 
 const Card = (props) =>  {
 
   return (
-      <div className="Card">
-        <Link to={'edit/'+ props.id}>
-           <h2 className="name">{props.name}</h2>
-           <h3 className="speed">{props.speed}</h3>
-           <p className="color">{props.color}</p>
-           <Link to={'edit/'+ props.id}><button>Edit Crewmate</button></Link>
+      <div className={`Card color-${props.color}`}>
+        <Link to={'details/'+ props.id} className="card-link">
+            <img src={crewmateOutline} alt="crewmate outline" className={`crewmate-outline ${props.type}`} />
+            <h2 className="name">Name: <span className='stat'>{props.name}</span></h2>
+            <h2 className="speed">Speed: <span className='stat'>{props.speed} mph</span></h2>
+            <h2 className="color">Color: <span className='stat'>{props.color}</span></h2>
+            <Link to={'/edit/'+ props.id}>
+                <button>Edit Crewmate</button>
+            </Link>
         </Link>
       </div>
   );
