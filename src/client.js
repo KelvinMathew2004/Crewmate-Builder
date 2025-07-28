60a2bf5474d16c057e06a9d45b4b4225abc73504
Kelvin Mathew <kelvinmathew2004@gmail.com>
2025-07-27 22:42:43 -0500

    Changed variable process for API keys

--- src/client.js
+++ src/client.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const URL = process.env.REACT_APP_SUPABASE_URL
-const API_KEY = process.env.REACT_APP_SUPABASE_ANON_KEY
+const URL = import.meta.env.VITE_SUPABASE_URL
+const API_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(URL, API_KEY)
